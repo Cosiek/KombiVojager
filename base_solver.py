07da1b8a2d0a8c8e28db3c9bed9de1d9f9a7ad6f
cosiek <witold.majchrzak@21s.pl>
2014-11-10 00:35:26 +0100

    Add base solver class

---
+++ base_solver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from datetime import datetime
+
+class BaseSolver(object):
+    task = None
+
+    best_solution = None
+    best_distance = float('inf')
+    search_time = None
+
+    def __init__(self, task):
+        self.task = task
+
+    def run(self):
+        start_time = datetime.now()
+        self.best_solution, self.best_distance = self.run_search()
+        finish_time = datetime.now()
+
+        self.search_time = finish_time - start_time
+
+    def run_search(self):
+        # dummy - this is where one should implement the algorithm
+        pass
