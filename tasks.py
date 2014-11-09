acee0e0a7c8906f77dfc82f7b6ecbac7eacaf818
cosiek <witold.majchrzak@21s.pl>
2014-11-10 00:45:55 +0100

    Add example task

---
+++ tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from base_task import BaseTask, Node
+
+task_data = {
+    'start': Node('A', 0, 0),
+    'finish': Node('Z', 500, 500),
+    'mid_nodes': [
+        Node('B', 50, 500),
+        Node('C', 50, 100),
+        Node('D', 0, 550),
+        Node('E', 500, 100),
+        Node('F', 250, 250),
+        Node('G', 150, 400),
+        Node('H', 350, 150),
+    ],
+}
+
+task = BaseTask(**task_data)
