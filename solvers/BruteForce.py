51ee19f41e6fc48d4791bde97c5d28d55d76cdf4
cosiek <witold.majchrzak@21s.pl>
2014-11-10 01:45:19 +0100

    Add brute force inplementation

---
+++ solvers/BruteForce.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from itertools import permutations
+
+from base_solver import BaseSolver
+
+class BruteForceSolver(BaseSolver):
+    def run_search(self):
+        # get list of mid nodes names
+        mid_nodes = []
+        for node in self.task.mid_nodes:
+            mid_nodes.append(node.name)
+
+        # iterate over permutations generator
+        best_distance = float('inf')
+        best_solution = None
+        cycles = 0
+        for permutation in permutations(mid_nodes):
+            # check permutation distance
+            path = [self.task.start.name, ]
+            path.extend(permutation)
+            path.append(self.task.finish.name)
+            distance = self.task.get_path_distance(path)
+
+            # check if this is the best solution so far
+            if distance < best_distance:
+                best_distance = distance
+                best_solution = path
+
+            cycles += 1
+
+        return best_solution, best_distance, cycles
