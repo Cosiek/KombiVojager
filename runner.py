9543a767ac496ee99464ef0454e4a8839e803230
cosiek <witold.majchrzak@21s.pl>
2014-11-10 19:05:36 +0100

    Add inplementation of naive solver
    A solver that adds a node, that's closest to the last node
    in current path.

--- runner.py
+++ runner.py
@@ -3,9 +3,12 @@
 
 from tasks import task
 from solvers.BruteForce import BruteForceSolver
+from solvers.Naive import NaiveSolver
 
 brutal = BruteForceSolver(task)
 brutal.run()
-
 print brutal.get_summary()
 
+naive = NaiveSolver(task)
+naive.run()
+print naive.get_summary()
---
+++ solvers/Naive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from base_solver import BaseSolver
+
+class NaiveSolver(BaseSolver):
+    def run_search(self):
+        # get list of mid nodes names
+        mid_nodes = []
+        for node in self.task.mid_nodes:
+            mid_nodes.append(node.name)
+
+        # find node that is closest to start
+        closest = self.pop_closest(self.task.start.name, mid_nodes)
+
+        # build solution
+        solution = [closest,]
+        while mid_nodes:
+            closest = self.pop_closest(solution[-1], mid_nodes)
+            solution.append(closest)
+
+        # add start and finish to solution
+        solution.append(self.task.finish.name)
+        solution.insert(0, self.task.start.name)
+
+        # calculate distance
+        distance = self.task.get_path_distance(solution)
+
+        return solution, distance, 1
+
+    def pop_closest(self, last, mid_nodes):
+        distance = float('inf')
+
+        i = 0
+        closest_idx = 0
+        for node_name in mid_nodes:
+            d = self.task.get_distance(last, node_name)
+            if d < distance:
+                distance = d
+                closest_idx = i
+            i += 1
+
+        return mid_nodes.pop(closest_idx)
