a489a0b296f3311a878b0cf7262724a966a86ed0
cosiek <witold.majchrzak@21s.pl>
2014-12-27 13:19:26 +0100

    Add helpers module

---
+++ helpers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+INF = float('inf')
+
+def print_array(array):
+    print '-----'
+    for row in array:
+        print [int(x) if isinstance(x, float) and x < INF else x for x in row]
