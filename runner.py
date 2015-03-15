39eb704d5d6b4a3b2c9e260058a0163beef3ef47
cosiek <witold.majchrzak@21s.pl>
2015-03-15 23:35:44 +0100

    Allow choice of task to be run, by passing arg to runner

--- runner.py
+++ runner.py
@@ -2,10 +2,14 @@
 # encoding: utf-8
 
 import pkgutil
+import sys
 
 from base_solver import BaseSolver
-from db_manage import connection, cursor
-from tasks import task
+import tasks
+
+
+class NoSuchTaskException(KeyError):
+    pass
 
 
 def run_solver(solver_class, task):
@@ -16,23 +20,39 @@ def run_solver(solver_class, task):
     print solver.get_summary()
 
 
-# iterate over solvers package to find classes of solvers
-for module_tuple in pkgutil.iter_modules(['solvers']):
-    module = __import__('solvers.' + module_tuple[1], fromlist=[''])
+if __name__ == '__main__':
+    # get task
+    task_name = ' '.join(sys.argv[1:])
+    try:
+        task = tasks.tasks[task_name]
+    except KeyError:
+        print >> sys.stderr, (
+                'Unknown task name: "%s".\n'
+                'This is a list of defined tasks:\n'
+                '%s' % (task_name, '\n'.join(tasks.tasks.keys()))
+                )
+        sys.exit(1)
 
-    for name in dir(module):
-        # skip private attributes
-        if name.startswith('__'):
-            continue
+    # prepare db connection to store results
+    from db_manage import connection, cursor
 
-        stuf = getattr(module, name)
-        # if something has 'a_solver' attr set to true, assume it is a solver
-        # and run it on task. Since BaseSolver also has 'a_solver' set to true
-        # it needs to be excluded.
-        if getattr(stuf, 'a_solver', False) and not stuf==BaseSolver:
-            run_solver(stuf, task)
+    # iterate over solvers package to find classes of solvers
+    for module_tuple in pkgutil.iter_modules(['solvers']):
+        module = __import__('solvers.' + module_tuple[1], fromlist=[''])
+
+        for name in dir(module):
+            # skip private attributes
+            if name.startswith('__'):
+                continue
+
+            stuf = getattr(module, name)
+            # if something has 'a_solver' attr set to true, assume it is a solver
+            # and run it on task. Since BaseSolver also has 'a_solver' set to true
+            # it needs to be excluded.
+            if getattr(stuf, 'a_solver', False) and not stuf==BaseSolver:
+                run_solver(stuf, task)
 
 
-# commit and let go of db
-connection.commit()
-connection.close()
+    # commit and let go of db
+    connection.commit()
+    connection.close()
--- tasks.py
+++ tasks.py
@@ -3,23 +3,28 @@
 
 from base_task import BaseTask, Node
 
-task_data = {
-    'name': 'first_test_runs_task',
-    'start': Node('A', 0, 0),
-    'finish': Node('Z', 500, 500),
-    'mid_nodes': [
-        Node('B', 50, 500),
-        Node('C', 50, 100),
-        Node('D', 0, 550),
-        Node('E', 500, 100),
-        Node('F', 250, 250),
-        Node('G', 150, 400),
-        Node('H', 350, 150),
-    ],
-    'distances': {
-        'A:B': 10,
+task_definitions = [
+    {
+        'name': 'first_test_runs_task',
+        'start': Node('A', 0, 0),
+        'finish': Node('Z', 500, 500),
+        'mid_nodes': [
+            Node('B', 50, 500),
+            Node('C', 50, 100),
+            Node('D', 0, 550),
+            Node('E', 500, 100),
+            Node('F', 250, 250),
+            Node('G', 150, 400),
+            Node('H', 350, 150),
+        ],
+        'distances': {
+            'A:B': 10,
+        },
+        'timeout': 1,
     },
-    'timeout': 1,
-}
+]
 
-task = BaseTask(**task_data)
+
+tasks = {}
+for task_def in task_definitions:
+    tasks[task_def['name']] = BaseTask(**task_def)
